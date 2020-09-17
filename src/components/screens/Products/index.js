21cddd20ca31164e03536b666ef607a0048c8c70
DavidPagliano <paglianodavid@gmail.com>
2020-09-16 22:53:06 -0300

    struture HTML and CSS complety

--- src/components/screens/Products/index.js
+++ src/components/screens/Products/index.js
@@ -15,9 +15,29 @@ class Products extends React.Component() {
       <div className="Products">
         <div className='wrapper'>
           <div className='header'><a href='#'>&larr; Back</a></div>
-          
+            <div className='game'>
+              <div id='board' className='board'>
+                {
+                  
+                }
+              </div>
+              <div className='ribbon'></div>
+              <div className='players'>
+                  <div className='spot yellow'></div>
+                  <div className='player p1'></div>
+                  <div className='direction left'></div>
+                  <div className='direction text'></div>
+                  <div className='direction right'></div>
+                  <div className='player p2'></div>
+                  <div className='spot red'></div>
+              </div>
+            </div>
+            <div className='footer'>
+                <button className='fb'>&#x1f44d; Like</button>
+                <button className='embed'>Embed</button>
+            </div>
         </div>
-          
+        
       </div>
     );
   }
